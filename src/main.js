95c30aa61f1857307c0ff47701dc3bd4fc6731af
fanzou2020 <fanzou2020@icloud.com>
2020-06-27 18:09:25 -0400

    initial commit

---
+++ src/main.js
@@ -0,0 +1,22 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import Axios from 'axios'
+import qs from 'qs'
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.css'
+
+Vue.prototype.$axios = Axios
+Vue.prototype.HOST = '/localhost:8081'
+
+// Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
